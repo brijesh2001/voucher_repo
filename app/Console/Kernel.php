767018a3b6b21bdfb24cb7b62e5557afeb68d5f0
Brijesh <bjpatel2001@gmail.com>
2018-04-27 14:23:10 +0530

    time changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,7 +37,7 @@ class Kernel extends ConsoleKernel
         //$schedule->command('update:invoice')
           //  ->everyTenMinutes();
         $schedule->command('send:referfriend')
-            ->dailyAt('10:00');
+            ->dailyAt('14:30');
     }
 
     /**
