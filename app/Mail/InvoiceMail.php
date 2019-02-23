d41d652dc35daf9036bb05a5de9aafab270870e8
unknown <pteexamvoucher@gmail.com>
2019-02-23 10:37:36 +0530

    change for invoice mail

--- app/Mail/InvoiceMail.php
+++ app/Mail/InvoiceMail.php
@@ -33,13 +33,17 @@ class InvoiceMail extends Mailable
 
         $address = 'info@ptevouchercode.com';
         $name = 'PTEVoucherCode.com';
+
+            $to_final_array = [];
             $to = $this->successData['email'];
+            $to_admin = 'info@ptevouchercode.com';
+            array_push($to_final_array,$to,$to_admin);
             $file = $this->successData['file_path'];
             $view = 'emails.invoice_send';
             $subject = 'PTE Voucher Code GST Invoice';
 
         return $this->view($view)
-            ->to($to)
+            ->to($to_final_array)
             ->from($address, $name)
             ->replyTo($address, $name)
             ->subject($subject)
