3effead8a067df79798ba3d068bcd6a19c5d21ac
brijesh <bjpatel2001@gmail.com>
2019-05-15 11:40:27 +0530

    changes for the invoice address sign die12

--- app/Http/Controllers/SaleDataController.php
+++ app/Http/Controllers/SaleDataController.php
@@ -317,13 +317,9 @@ class SaleDataController extends Controller
                     $data['voucher_code'] = str_replace(',', '<br />', $data['voucher_code']);
                     $data['word_amount'] = $this->getIndianCurrency($data['amount_paid']);
                     $data['created_at'] = date("d-m-Y", strtotime($data['created_at']));
-                    echo $data['created_at'];
-                    echo'<br>';
-                    echo"-";
-                    echo date('Y-m-d 00:00:00', strtotime(trim($data['created_at'])));
-                    die();
+                    
                     $check_date_for_older_invoice = date('Y-m-d 00:00:00', strtotime(trim('13-05-2019')));
-                    if($check_date_for_older_invoice < date('Y-m-d 00:00:00', strtotime(trim($data['created_at'])))){
+                    if(date('Y-m-d 00:00:00', strtotime(trim($data['created_at']))) < $check_date_for_older_invoice){
                         $pdf = PDF::loadView('emails.invoice', $data);
                     }else{
                         $pdf = PDF::loadView('emails.new_invoice', $data);
