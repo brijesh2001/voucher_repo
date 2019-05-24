70318b5fc74840ef2e7c69b396be84a62d6c2abb
brijesh <bjpatel2001@gmail.com>
2019-05-24 12:32:10 +0530

    change in the pricie

--- resources/views/front/buy.blade.php
+++ resources/views/front/buy.blade.php
@@ -84,7 +84,8 @@
                                 <input type="hidden" name="user_id" value="{{$user_id or ''}}">
                                 {{ csrf_field() }}
                                 <div class="col-md-12">
-                                    <p style="margin-bottom: 0px;margin-top: -7px">PTE Voucher Price : 10339 + 1861 (18% GST) = {{$rate or ''}} INR</p>
+                                    <p style="margin-bottom: 0px;margin-top: -7px">PTE Voucher Price : 10381 + 1869 (18%
+                                        GST) = {{$rate or ''}} INR</p>
                                     <div class="submit-button text-center">
                                         <button class="btn btn-common1" id="submit" type="submit" style="cursor: pointer">Buy Now</button>
                                         <div id="msgSubmit" class="h3 text-center hidden"></div>
