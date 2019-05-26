42e491dcef2a9c85567a14a9287a71aeb40ed3d5
brijesh <bjpatel2001@gmail.com>
2019-05-26 11:26:08 +0530

    client gstn

--- resources/views/emails/admin_enquiry.blade.php
+++ resources/views/emails/admin_enquiry.blade.php
@@ -2,6 +2,8 @@
 Customer Mobile  {{$enquiryData->mobile}}<br/>
 Customer Email   {{$enquiryData->email}}<br/>
 State {{$state}}<br/>
-Number Of Voucher      {{$enquiryData->number_of_voucher}} <br/><br/><br/><br/>
+Number Of Voucher      {{$enquiryData->number_of_voucher}} <br/>
+Client Gstn : {{$enquiryData->client_gstn}}
+<br/><br/><br/>
 Cheers,<br/>
 Hitesh Patel<br/>
